2069e00e0df278e753391977ae7dcbc1b49b16b3
MathheeuusS <86687276+marie006@users.noreply.github.com>
2024-06-26 11:29:22 -0300

    Site final graças a Deus

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var a = document.getElementById("botaun")
+var b = document.getElementById("lista")
+var c = document.getElementById("menuGambiarraMalu")
+
+function botao()
+{
+            b.style.display = "block";
+            c.style.display = "block"
+            a.style.display = "none"
+}
+
+function fechar()
+{{
+    b.style.display = "none"
+    c.style.display = "none"
+    a.style.display = "block"
+}}
